fc8eccabc6434bdbc3b65dbbb7d0e1e457082055
Roman Stetsiuk <romansteciuk021@gmail.com>
2018-10-27 09:42:54 +0200

    Create method drawing line by size 10

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import sun.java2d.loops.DrawLine;
+
+public class Main {
+
+//    Method draw line witch size 10
+    public static void DrawLine () {
+        for (int i = 0; i < 10; i++ ) {
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+
+
+
+    public static void main(String[] args) {
+        DrawLine();
+    }
+
+}
