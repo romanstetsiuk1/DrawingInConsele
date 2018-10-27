5fd1bf346ef141d5b3b12dceecf5ab24449bb911
Roman Stetsiuk <romansteciuk021@gmail.com>
2018-10-27 10:07:29 +0200

    Add method drawing empty box

--- src/Main.java
+++ src/Main.java
@@ -21,9 +21,25 @@ public class Main {
         System.out.println();
     }
 
+//    Draw empty box with size 10x10
+    public static void EmtyBox(){
+        for (int i = 0; i < 10; i++ ) {
+            System.out.println();
+            for (int j = 0; j < 10; j++) {
+                if ( i == 0 || i == 9 || j == 0 || j == 9 ) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+        }
+        System.out.println();
+    }
+
     public static void main(String[] args) {
         DrawLine();
         DrawBox();
+        EmtyBox();
     }
 
 }
